0c3f585abd18dbd36a4470a49287ccf8c20d63b8
alexisgonzale <goez-walter@hotmail.com>
2018-10-22 13:17:44 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,31 @@
 
-
+// ===========================
 //Puerto
+// ===========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ===========================
+// Entorno
+// ==========================
+
+
+process.env.NODE_ENV = process.env.NODE_ENV  || 'dev';
+
+//===============================
+// base de datos 
+//==============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else {
+    urlDB = 'mongodb://cafe-user:123456FS@ds237713.mlab.com:37713/cafanode';
+}
+
+
+process.env.URLDB = urlDB;
+
+
+
